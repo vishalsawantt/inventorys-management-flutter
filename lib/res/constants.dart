70f0c0ea2a4b1b1ce1bf946ddf1031630264967a
VISHAL <sawantvishal2001@gmail.com>
2026-01-17 22:27:14 +0530

    live on netlify

--- lib/res/constants.dart
+++ lib/res/constants.dart
@@ -1,3 +1,5 @@
 class AppConstants {
-  static const String baseUrl = "http://localhost:8080";
+  //static const String baseUrl = "http://localhost:8080";
+  static const String baseUrl =
+      "https://inventory-management-springboot.onrender.com";
 }
